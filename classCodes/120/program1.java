7c474662c85cdfa7e12959591ef1d5b66590c38b
Adityakaldhone <adityakaldhone1357@gmail.com>
2023-12-25 15:24:52 +0530

    Stack Implementation

---
+++ classCodes/120/program1.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class StackDemo {
+
+	public static void main(String[]args) {
+	
+		Stack<Integer> s = new Stack<Integer>();
+
+		s.push(10);
+		s.push(20);
+		s.push(30);
+		s.push(40);
+
+		System.out.println(s);
+
+		System.out.println(s.pop());
+		
+		System.out.println(s);
+		
+		System.out.println(s.peek());
+		
+		System.out.println(s);
+
+		System.out.println(s.empty());
+
+	}
+}
---
+++ classCodes/120/program2.java
@@ -0,0 +1,181 @@
+import java.util.*;
+
+class Stack {
+
+	int maxSize;
+	int stackArr[];
+	int top = -1;
+
+	Stack(int Size) {
+	
+		this.stackArr = new int[Size];
+		this.maxSize = Size;
+	}
+	void push(int data) {
+	
+		if(top == maxSize-1) {
+		
+			System.out.println("Stack overFlow");
+			return;
+		}else {
+		
+			top++;
+			stackArr[top] = data;
+		}
+	}
+	boolean empty() {
+	
+		if(top == -1){
+		
+			return true;
+		}else {
+		
+			return false;
+		}
+	}
+
+	int pop() {
+	
+		if(empty()) {
+		
+			System.out.println("Stack is empty");
+			return -1;
+		}else{
+		
+			int val = stackArr[top];
+			top--;
+			return val;
+		}		
+	}
+	int peek() {
+	
+
+		if(empty()) {
+		
+			System.out.println("Stack is empty");
+			return -1;
+		}else{
+		
+			return stackArr[top];
+		}		
+	}
+	int size() {
+		
+		return top;
+	}
+	void printStack() {
+	
+		if(empty())  {
+		
+			System.out.println("Stack is empty");
+			return;
+		}else {
+		
+			System.out.print("[");
+			for(int val = 0;val<=top;val++) {
+			
+				System.out.print(stackArr[val] + ", ");
+			}
+			System.out.print("]");
+			System.out.println();
+		}
+	}
+
+}
+class Client {
+
+	public static void main(String[]args) {
+	
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter stack size");
+		int size = sc.nextInt();
+
+		Stack s = new Stack(size);
+
+		char ch;
+
+		do {
+		
+			System.out.println("1.push");
+			System.out.println("2.pop");
+			System.out.println("3.peek");
+			System.out.println("4.empty");
+			System.out.println("5.size");
+			System.out.println("6.printStack");
+
+			System.out.println("Enter choice");
+			int choice = sc.nextInt();
+
+			switch(choice) {
+			
+				case 1:
+					{
+					
+						System.out.println("Enter data");
+						int data = sc.nextInt();
+						s.push(data);
+					}
+					break;
+
+				case 2:
+					{
+					
+						int flag = s.pop();
+						if(flag != -1) {
+						
+							System.out.println(flag +"Popped");
+						}
+
+					}
+					break;
+
+				case 3:
+					{
+					
+						int flag = s.peek();
+						
+						if(flag != -1) {
+						
+							System.out.println(flag);
+						}
+					}
+					break;
+
+				case 4:
+					{
+					
+						boolean val = s.empty();
+						if(val){
+						
+							System.out.println("Empty stack");
+						}else {
+						
+							System.out.println("Not empty");
+						}
+					}
+					break;
+
+				case 5:
+					{
+					
+						int sz = s.size();
+						System.out.println("stack size" + sz+1);
+						
+					}
+					break;
+
+				case 6:
+					s.printStack();
+					break;
+
+				default:
+					System.out.println("Wrong choice");
+					break;
+
+			}
+			System.out.println("Do you want to continue? yes/no");
+			ch = sc.next().charAt(0);
+
+		}while(ch == 'Y' || ch == 'y');
+	}
+}
